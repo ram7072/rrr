7ba14e90d7f269586eebe861c417cdc2e0d2737b
ram7072 <rjraman100@gmail.com>
2022-03-11 12:13:41 +0530

    added

--- testspark.py
+++
@@ -1,6 +0,0 @@
-import pyspark
-from pyspark.sql import SparkSession
-from pyspark.sql.window import Window
-from pyspark.sql.functions import *
-# #export PYSPARK_PYTHON=python 3.7
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
